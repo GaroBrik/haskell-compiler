cf7e6cbca29c2c9e85fb7f521759521f370a2efc
Garo Brik <garo.brik@gmail.com>
2016-02-11 09:39:48 -0500

    Add pointer type and more runtime type information

--- Types.hs
+++ Types.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE GADTs #-}
+
 module Types (
   Type,
   getType,
   showType,
-  showVal
+  showVal,
+  hasResult,
+  Pointer(MkPointer)
 ) where
 
 import Data.Char
@@ -14,22 +21,34 @@ import Data.Char
 -- as this type class in addition to the definitions of "Code"s provides a sort
 -- of weak dependent typing.
 
-class Show a => Type a where
+class (Show a, Eq a) => Type a where
   showType :: a -> String
   showVal :: a -> String
   getType :: Type a => a
+  hasResult :: a -> Bool
   
 instance Type Bool where
   showType _ = "i1"
   showVal = map toLower . show
   getType = True
+  hasResult _ = True
 
 instance Type Int where
   showType _ = "i32"
   showVal = show
   getType = 0
+  hasResult _ = True
 
 instance Type () where
   showType _ = "void"
   showVal _ = "voidVal"
   getType = ()
+  hasResult _ = False
+
+newtype Pointer a = MkPointer a deriving (Eq, Show)
+
+instance forall a. Type a => Type (Pointer a) where
+  showType (MkPointer a) = '*':showType a
+  showVal _ = error "show value of pointer"
+  getType = MkPointer (getType :: a)
+  hasResult _ = True
